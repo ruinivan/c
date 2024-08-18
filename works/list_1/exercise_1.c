e12cac5b7f4fda9c71785f1a2a7178823296bb71
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-08-18 03:27:03 -0300

    Exercise 1
    Calculando o antecessor.

---
+++ works/list_1/exercise_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(0, "Portuguese");
+    int a, b;
+    printf("Escreva um número: ");
+    scanf("%d", &a);
+    b=--a;
+    printf("O antecessor do seu número é %d", b);
+    return 0;
+}
