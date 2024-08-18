7dbf74449665e94f981753555aa8ef0e77729359
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-08-18 02:18:41 -0300

    Exercise 7 finished
    Salario total em dois cenarios, o primeiro mais condizente com a questao e o segundo mais relacionado com a realidade e a pratica.

---
+++ works/list_1/exercise_7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(0, "Portuguese");
+    int carros;
+    float salario_fixo, comissao, valor_vendas, salario_total;
+    printf("Qual é o seu salario fixo? ");
+    scanf("%f", &salario_fixo);
+    printf("Quanto de comissão você ganha por carro vendido? ");
+    scanf("%f", &comissao);
+    printf("Quantos carros você vendeu? ");
+    scanf("%d", &carros);
+    printf("Qual é o total valor de vendas que você efetuadou ao todo? ");
+    scanf("%f", &valor_vendas);
+    salario_total = salario_fixo + comissao*carros + 0.05*valor_vendas;
+    printf("O seu salário total será de: R$%.2f", salario_total);
+    return 0;
+}
