fe33e8cb0c6fc18d60c07d48b71689dd44e91ee3
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-08-18 02:40:53 -0300

    Exercise 5
    Reajuste de salario

---
+++ works/list_1/exercise_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(0, "Portuguese");
+    int percentual_reajuste;
+    float salario_mensal, salario_final;
+    printf("Qual é o seu salário mensal, antes do reajuste? ");
+    scanf("%f", &salario_mensal);
+    printf("Qual é o perceltual de reajuste?(sem a porcentagem) ");
+    scanf("%d", &percentual_reajuste);
+    salario_final = salario_mensal*(1 + percentual_reajuste*0.01);
+    printf("O novo salario é de R$%.2f", salario_final);
+    return 0;
+}
