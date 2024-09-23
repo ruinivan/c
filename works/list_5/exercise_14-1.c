58f8b45c0f6f073ed36ca9ce0c6e2bf88da1c263
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-09-23 14:36:30 -0300

    fiz a determinante da 14

--- works/list_5/exercise_14-1.c
+++ works/list_5/exercise_14-1.c
@@ -6,7 +6,7 @@
 int main(){
     setlocale(LC_ALL,"Portuguese");
     int m[3][3];
-    int i, j;
+    int i, j, e, d, det;
     srand(time(NULL));
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
@@ -20,5 +20,9 @@ int main(){
         }
         printf("\n");
     }
+    d=(m[0][0]*m[1][1]*m[2][2])+(m[0][1]*m[1][2]*m[2][0])+(m[0][2]*m[1][0]*m[2][1]);
+    e=(m[0][2]*m[1][1]*m[2][0])+(m[0][0]*m[1][2]*m[2][1])+(m[0][1]*m[1][0]*m[2][2]);
+    det=d-e;
+    printf("O valor da determinante é: %d", det);
     return 0;
 }
--- works/list_5/exercise_14.c
+++ works/list_5/exercise_14.c
@@ -4,7 +4,7 @@
 int main(){
     setlocale(LC_ALL,"Portuguese");
     float m[3][3];
-    int i, j;
+    int i, j, d, e, det;
     for(i=0;i<3;i++){
         printf("%dª Coluna: \n", i+1);
         for(j=0;j<3;j++){
@@ -19,5 +19,9 @@ int main(){
         }
         printf("\n");
     }
+    d=(m[0][0]*m[1][1]*m[2][2])+(m[0][1]*m[1][2]*m[2][0])+(m[0][2]*m[1][0]*m[2][1]);
+    e=(m[0][2]*m[1][1]*m[2][0])+(m[0][0]*m[1][2]*m[2][1])+(m[0][1]*m[1][0]*m[2][2]);
+    det=d-e;
+    printf("A determinante da Matriz é: %d", det);
     return 0;
 }
