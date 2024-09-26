9576dd4e4593da2d412d667ba501b49d9a91fca9
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-09-26 13:28:20 -0300

    work_n1

--- works/work_n1/work_n1_3-1.c
+++ works/work_n1/work_n1_3-1.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <locale.h>
+#include "remove.h"
 
 int main(){
-    setlocale(LC_ALL, "Portuguese");
-    float b, x, a0, a1, a2, a3, a4, a5, a6, a7;
-    char v[101], vaux[2];
-    int i;
-    long int vr[50];
-    printf("Informe o código: ");
-    fgets(v, 100, stdin);
-    v[strlen(v)-1]='\0';
-    for(i=0;i<(int)strlen(v);i++){
-        vaux[0]=v[i];
-        vaux[1]=v[i+1];
-        if(v[i]=='0' && v[i+1]=='0'){
-            vr[i/2]='\0';
-            break;
+    char v[101], vaux[3];
+    int n=0, m, b, i, value;
+    do{
+        printf("Quantos codigos: ");
+        scanf("%d", &n);
+        if(n<=0 || n>10000) printf("A quantia pode estar somente entre 0 e 10000\n");
+    }while(n<=0 || n>10000);
+    for(m=0;m<n;m++){
+        do{
+            printf("Informe o código da inteligência: ");
+            scanf("%d", &b);
+            if(b<-100 || b>100) printf("O código de inteligência sóo pode entre entre -100 e 100\n");
+        }while(b<-100 || b>100);
+        printf("Informe o código: ");
+        fgets(v, 101, stdin);
+        v[strlen(v)-1]='\0';
+        for(i=0;i<(int)strlen(v);i+=2){
+            vaux[0]=v[i];
+            vaux[1]=v[i+1];
+            if(v[i]=='0' && v[i+1]=='0'){
+                break;
+            }
+            value=strtol(vaux, NULL, 16);
+            if(func_val(i/2+1, b)!=0) printf("%c", value);
         }
-        vr[i/2]=strtol(vaux, NULL, 16);
-    }
-    for(i=0;i<50;i++){
-        if(vr[i]=='\0') break;
-        printf("%c", vr[i]);
     }
     return 0;
 }
