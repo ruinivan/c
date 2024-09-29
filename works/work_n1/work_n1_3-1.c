583a5dbe266821ab310225b100e06492bf73223c
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-09-29 12:06:35 -0300

    aperfeiçoando o trabalho

--- works/work_n1/work_n1_3-1.c
+++ works/work_n1/work_n1_3-1.c
@@ -6,7 +6,7 @@
 int main(){
     char codigo[101], aux[3];
     int quantidade=0, b, value;
-    system("chcp 850 > nul");
+    system("chcp 850 > nul"); //A tabela ascii por padrao nao contem totalmente as nossas acentuacoes e nossas letras
     do{
         printf("Quantos codigos: ");
         scanf("%d", &quantidade);
@@ -17,18 +17,18 @@ int main(){
             printf("Informe o codigo da inteligencia: ");
             scanf("%d", &b);
             getchar(); //Limpar o buffer
-            if(b<-100 || b>100) printf("O codigo de inteligencia so pode entre entre -100 e 100\n");
-        }while(b<-100 || b>100);
+            if(b< -100 || b>100) printf("O codigo de inteligencia so pode entre entre -100 e 100\n");
+        }while(b< -100 || b>100);
         printf("Informe o codigo: ");
         fgets(codigo, 101, stdin);
-        codigo[strcspn(codigo, "\n")] = 0; //Maneira melhor de tirar o enter da string e finalizar ela
+        codigo[strcspn(codigo, "\n")] = "\0"; //Maneira melhor de tirar o enter da string e finalizar ela
         for(int i=0;i<(int)strlen(codigo);i+=2){
             aux[0]=codigo[i];
             aux[1]=codigo[i+1];
             aux[2] = '\0'; //Finalizar a string
             if(codigo[i]=='0' && codigo[i+1]=='0') break;
             value=strtol(aux, NULL, 16);
-            if(func_val((i/2)+1, b)!=0 && ((value >= 32 && value <= 126) || (value >= 128 && value <= 255))) printf("%c", (char)value);
+            if(func_val((i/2)+1, b)!=0 && ((value >= 32 && value <= 126) || (value >= 128 && value <= 255))) printf("%c", (char)value); //A função exclui caracteres indesejados e os outros dois parametros do if confirma que o caracter esta dentro dos desejados
         }
         printf("\n");
     }
