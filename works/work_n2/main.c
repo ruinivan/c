82cbc88550d4d7d43e2f46d8182bf57b02c2f661
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-11-05 19:54:19 -0300

    começando o projeto

---
+++ works/work_n2/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "pacman.h"
+
+int main(){
+    score.pontos = 0;
+    moedas.valor = 100;
+    pacman.poder = 0;
+    fruta.poder = &pacman.poder;
+    fantasma.morrer = &pacman.poder;
+
+    do{
+        pacman.local.y++;           /* andar pra cima */
+    }while(pacman.andar=='w');
+    do{
+        pacman.local.x--;           /* andar pra esquerda */
+    }while(pacman.andar=='a');
+    do{
+        pacman.local.y++;           /* andar pra baixo */
+    }while(pacman.andar=='s');
+    do{
+        pacman.local.x++;           /* andar pra direita */
+    }while(pacman.andar=='d');
+
+    if(((pacman.local.x == fantasma1.local.x && pacman.local.y == fantasma1.local.y) || (pacman.local.x == fantasma2.local.x && pacman.local.y == fantasma2.local.y) || (pacman.local.x == fantasma3.local.x && pacman.local.y == fantasma3.local.y) || (pacman.local.x == fantasma4.local.x && pacman.local.y == fantasma4.local.y) || (pacman.local.x == fantasma5.local.x && pacman.local.y == fantasma5.local.y) || (pacman.local.x == fantasma6.local.x && pacman.local.y == fantasma6.local.y)) && pacman.poder == 0) /*GAME OVER*/ {
+        
+    }
+
+    if((pacman.local.x == fantasma1.local.x && pacman.local.y == fantasma1.local.y) && pacman.poder == 1) /*Kill fantasma1*/ {
+
+    }
+    if((pacman.local.x == fantasma2.local.x && pacman.local.y == fantasma2.local.y) && pacman.poder == 1) /*Kill fantasma2*/ {
+
+    }
+    if((pacman.local.x == fantasma3.local.x && pacman.local.y == fantasma3.local.y) && pacman.poder == 1) /*Kill fantasma3*/ {
+
+    }
+    if((pacman.local.x == fantasma4.local.x && pacman.local.y == fantasma4.local.y) && pacman.poder == 1) /*Kill fantasma4*/ {
+
+    }
+    if((pacman.local.x == fantasma5.local.x && pacman.local.y == fantasma5.local.y) && pacman.poder == 1) /*Kill fantasma5*/ {
+
+    }
+    if((pacman.local.x == fantasma6.local.x && pacman.local.y == fantasma6.local.y) && pacman.poder == 1) /*Kill fantasma6*/ {
+
+    }
+    return 0;
+}
---
+++ works/work_n2/pacman.h
@@ -0,0 +1,57 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+typedef struct{
+    int x;
+    int y;
+}posicao;
+
+struct{
+    posicao local;
+    char andar;
+    int poder;
+}pacman;
+
+struct{
+    int *morrer;
+}fantasma;
+
+struct{
+    posicao local;
+}fantasma1;
+
+struct{
+    posicao local;
+}fantasma2;
+
+struct{
+    posicao local;
+}fantasma3;
+
+struct{
+    posicao local;
+}fantasma4;
+
+struct{
+    posicao local;
+}fantasma5;
+
+struct{
+    posicao local;
+}fantasma6;
+
+struct{
+    int pontos;
+}score;
+
+struct{
+    posicao local;
+    int valor;
+}moedas;
+
+struct{
+    posicao local;
+    int *poder;
+}fruta;
+
+#endif
