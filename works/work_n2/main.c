55ab9341a2127bb082ba3640861205eba08e1965
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-11-05 20:21:24 -0300

    ajustes

--- works/work_n2/main.c
+++ works/work_n2/main.c
@@ -8,40 +8,40 @@ int main(){
     fruta.poder = &pacman.poder;
     fantasma.morrer = &pacman.poder;
 
-    do{
-        if(pacman.local.y-1 != parede.local.x) pacman.local.y--;           /* andar pra cima */
-    }while(pacman.andar=='w');
-    do{
-        if(pacman.local.x-1 != parede.local.x) pacman.local.x--;           /* andar pra esquerda */
-    }while(pacman.andar=='a');
-    do{
-        if(pacman.local.y+1 != parede.local.x) pacman.local.y++;           /* andar pra baixo */
-    }while(pacman.andar=='s');
-    do{
-        if(pacman.local.x+1 != parede.local.x) pacman.local.x++;           /* andar pra direita */
-    }while(pacman.andar=='d');
+    while((pacman.local.y-1 != parede.local.y && pacman.local.x != parede.local.x) && pacman.andar=='w'){
+        pacman.local.y--;       /* andar pra cima */
+    }
+    while((pacman.local.x+1 != parede.local.x && pacman.local.y != parede.local.y) && pacman.andar=='a'){
+        pacman.local.x--;       /* andar pra esquerda */
+    }
+    while((pacman.local.y+1 != parede.local.y && pacman.local.x != parede.local.x) && pacman.andar=='s'){
+        pacman.local.y++;       /* andar pra baixo */
+    }
+    while((pacman.local.x+1 != parede.local.x && pacman.local.y != parede.local.y) && pacman.andar=='d'){
+        pacman.local.x++;       /* andar pra direita */
+    }
 
     if(((pacman.local.x == fantasma1.local.x && pacman.local.y == fantasma1.local.y) || (pacman.local.x == fantasma2.local.x && pacman.local.y == fantasma2.local.y) || (pacman.local.x == fantasma3.local.x && pacman.local.y == fantasma3.local.y) || (pacman.local.x == fantasma4.local.x && pacman.local.y == fantasma4.local.y) || (pacman.local.x == fantasma5.local.x && pacman.local.y == fantasma5.local.y) || (pacman.local.x == fantasma6.local.x && pacman.local.y == fantasma6.local.y)) && pacman.poder == 0) /*GAME OVER*/ {
         
     }
 
     if((pacman.local.x == fantasma1.local.x && pacman.local.y == fantasma1.local.y) && pacman.poder == 1) /*Kill fantasma1*/ {
-
+        score.pontos += 150;
     }
     if((pacman.local.x == fantasma2.local.x && pacman.local.y == fantasma2.local.y) && pacman.poder == 1) /*Kill fantasma2*/ {
-
+        score.pontos += 150;
     }
     if((pacman.local.x == fantasma3.local.x && pacman.local.y == fantasma3.local.y) && pacman.poder == 1) /*Kill fantasma3*/ {
-
+        score.pontos += 150;
     }
     if((pacman.local.x == fantasma4.local.x && pacman.local.y == fantasma4.local.y) && pacman.poder == 1) /*Kill fantasma4*/ {
-
+        score.pontos += 150;
     }
     if((pacman.local.x == fantasma5.local.x && pacman.local.y == fantasma5.local.y) && pacman.poder == 1) /*Kill fantasma5*/ {
-
+        score.pontos += 150;
     }
     if((pacman.local.x == fantasma6.local.x && pacman.local.y == fantasma6.local.y) && pacman.poder == 1) /*Kill fantasma6*/ {
-
+        score.pontos += 150;
     }
 
     return 0;
