794ba79ae14046af9e2e281bcf1b63a7e4eabb32
Victor Ruinivan <victorruinivanvg@gmail.com>
2024-11-24 23:53:07 -0300

    fim

--- works/work_n2/main.c
+++ works/work_n2/main.c
@@ -3,229 +3,277 @@
 #include <time.h>
 #include <windows.h>
 #include <conio.h>
-#include "pacman.h"
 
-void carregar_fase(int fase, int parede[10][35]) {
-    // Fase 1
-    if (fase == 0) {
-        for (int i = 8; i < 16; i++) {
-            parede[12][i] = 1;
-        }
-    } 
-    // Fase 2
-    else if (fase == 1) {
-        for (int i = 5; i < 20; i++) {
-            parede[i][10] = 1;
-            parede[i][15] = 1;
-        }
-    } 
-    // Fase 3
-    else if (fase == 2) {
-        for (int i = 5; i < 20; i++) {
-            parede[5][i] = 1; 
-            parede[20][i] = 1;
-        }
-        for (int i = 10; i < 15; i++) {
-            parede[i][5] = 1;
-            parede[i][20] = 1;
+#define ALTURA 10
+#define LARGURA 20
+#define FASES 5
+
+typedef struct {
+    int x, y;
+} coordenadas;
+
+typedef struct {
+    coordenadas posicao;
+    int vivo;
+} fantasma_t;
+
+typedef struct {
+    coordenadas posicao;
+    int poderoso;
+    int vidas;
+} pacman_t;
+
+// Mapas das fases
+char mapas[FASES][ALTURA][LARGURA] = {
+    {
+        "###################",
+        "#........#........#",
+        "#.####.#.#.####.#.#",
+        "#o#  #.#.#.#  #.#o#",
+        "#.####.###.####.#.#",
+        "#.................#",
+        "###.###.#.#.###.###",
+        "  #.# o.#.#.o.#.#  ",
+        "  #.# #########.#  ",
+        "###################"
+    },
+    {
+        "###################",
+        "#........#........#",
+        "#.##o..#.#.#....#.#",
+        "#.#  #.#.#.#.o..#.#",
+        "#.####.###.####.#.#",
+        "#o...............o#",
+        "###.###.#.#.###.###",
+        "#.#.o..#.#.#.o..#.#",
+        "#.#.###########.#.#",
+        "###################"
+    },
+    {
+        "###################",
+        "#...#..........#..#",
+        "#.#.###.#####.#.###",
+        "#.#...........#...#",
+        "#.###.#.#######.#.#",
+        "#..o..#...o.....#.#",
+        "#.###########.#.###",
+        "#.#..........#.#..#",
+        "#.#.#######o.#.o.#o",
+        "###################"
+    },
+    {
+        "###################",
+        "#........#........#",
+        "#o####.###.####o#.#",
+        "#.#..#.....#...#.#o",
+        "#.#.####.####.###.#",
+        "#.#...........#...#",
+        "#.#.###########.#.#",
+        "#.#...........o#.#o",
+        "#.#.###########.#.#",
+        "###################"
+    },
+    {
+        "###################",
+        "#.....#.#.........#",
+        "#.#.#.#.#.#.#####.#",
+        "#.#.#.#o#.#.#o..#.#",
+        "#.#.#.###.#.#####.#",
+        "#.#...........#...#",
+        "#.#####.#.#####.#.#",
+        "#.#o....#.......#.#",
+        "#.#.###########.#o#",
+        "###################"
+    }
+};
+
+fantasma_t fantasmas[4];
+pacman_t pacman;
+
+int fase_atual = 0;
+int pontos = 0;
+int moedas_restantes = 0;
+int frutas_tempo = 0;
+
+void inicializar_jogo() {
+    // Inicializa Pac-Man
+    pacman.posicao.x = 1;
+    pacman.posicao.y = 1;
+    pacman.poderoso = 0;
+    pacman.vidas = 3;
+
+    // Inicializa fantasmas
+    fantasmas[0].posicao.x = 5;
+    fantasmas[0].posicao.y = 10;
+    fantasmas[1].posicao.x = 5;
+    fantasmas[1].posicao.y = 9;
+    fantasmas[2].posicao.x = 5;
+    fantasmas[2].posicao.y = 11;
+    fantasmas[3].posicao.x = 5;
+    fantasmas[3].posicao.y = 8;
+    for (int i = 0; i < 4; i++) {
+        fantasmas[i].vivo = 1;
+    }
+
+    // Conta moedas no mapa da fase atual
+    moedas_restantes = 0;
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            if (mapas[fase_atual][i][j] == '.') {
+                moedas_restantes++;
+            }
         }
     }
 }
 
-void inicializar_moedas(moedas *moeda[], int quant_moeda, int parede[10][35]) {
-    // Garante que temos as 50 moedas
-    for (int i = 0; i < quant_moeda; i++) {
-        moeda[i] = (moedas *)malloc(sizeof(moedas));
-        do {
-            moeda[i]->local.x = rand() % 9 + 1; // Gera valores entre 1 e 8 para x
-            moeda[i]->local.y = rand() % 33 + 1; // Gera valores entre 1 e 33 para y
-        } while (parede[moeda[i]->local.x][moeda[i]->local.y] == 1); // Evita colocar moedas em paredes
-    }
-}
+void desenhar_mapa() {
+    system("cls");
 
-void inicializar_frutas(frutas *fruta[], int quant_frutas, int parede[10][35]) {
-    for (int i = 0; i < quant_frutas; i++) {
-        fruta[i] = (frutas *)malloc(sizeof(frutas));
-        do {
-            fruta[i]->local.x = rand() % 25;
-            fruta[i]->local.y = rand() % 25;
-        } while (parede[fruta[i]->local.x][fruta[i]->local.y] == 1);
-    }
-}
+    for (int i = 0; i < ALTURA; i++) {
+        for (int j = 0; j < LARGURA; j++) {
+            int desenhado = 0;
 
-void desenhar_mapa(int parede[10][35], fantasmas fantasma[], moedas *moeda[], frutas *fruta[], int quant_moeda, int quant_frutas) {
-    system("cls"); // Limpa o console no Windows
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 35; j++) {
-            if (parede[i][j] == 1) {
-                printf("#"); // Parede
-            } else {
-                int desenhado = 0;
-                // Verifica fantasmas
-                for (int f = 0; f < 4; f++) {
-                    if (fantasma[f].local.x == i && fantasma[f].local.y == j && fantasma[f].vida) {
-                        printf("F");
-                        desenhado = 1;
-                        break;
-                    }
-                }
-                // Verifica moedas
-                for (int m = 0; m < quant_moeda; m++) {
-                    if (!desenhado && moeda[m] && moeda[m]->local.x == i && moeda[m]->local.y == j) {
-                        printf("o");
-                        desenhado = 1;
-                    }
-                }
-                // Verifica frutas
-                for (int fr = 0; fr < quant_frutas; fr++) {
-                    if (!desenhado && fruta[fr] && fruta[fr]->local.x == i && fruta[fr]->local.y == j) {
-                        printf("0");
-                        desenhado = 1;
-                    }
-                }
-                // Verifica Pacman
-                if (!desenhado && pacman.local.x == i && pacman.local.y == j) {
-                    printf("@");
+            // Verifica se há fantasmas
+            for (int f = 0; f < 4; f++) {
+                if (fantasmas[f].vivo && fantasmas[f].posicao.x == i && fantasmas[f].posicao.y == j) {
+                    printf("F");
                     desenhado = 1;
+                    break;
                 }
-                // Espaço vazio
-                if (!desenhado)
-                    printf(" ");
+            }
+
+            // Verifica posição do Pac-Man
+            if (!desenhado && pacman.posicao.x == i && pacman.posicao.y == j) {
+                printf(pacman.poderoso ? "P" : "C");
+                desenhado = 1;
+            }
+
+            // Desenha o mapa
+            if (!desenhado) {
+                printf("%c", mapas[fase_atual][i][j]);
             }
         }
         printf("\n");
     }
+
+    printf("Pontos: %d  Vidas: %d  Fase: %d\n", pontos, pacman.vidas, fase_atual + 1);
 }
-void mover_pacman(int parede[10][35]) {
+
+void mover_pacman() {
     if (_kbhit()) {
         char tecla = _getch();
+        int novo_x = pacman.posicao.x;
+        int novo_y = pacman.posicao.y;
+
+        // Processa entrada do jogador
         switch (tecla) {
-            case 'w': // Cima
-                if (pacman.local.x > 0 && parede[pacman.local.x - 1][pacman.local.y] == 0)
-                    while(tecla == 'w') pacman.local.x--;
-                break;
-            case 'a': // Esquerda
-                if (pacman.local.y > 0 && parede[pacman.local.x][pacman.local.y - 1] == 0)
-                    pacman.local.y--;
-                break;
-            case 's': // Baixo
-                if (pacman.local.x < 9 && parede[pacman.local.x + 1][pacman.local.y] == 0)
-                    pacman.local.x++;
-                break;
-            case 'd': // Direita
-                if (pacman.local.y < 49 && parede[pacman.local.x][pacman.local.y + 1] == 0)
-                    pacman.local.y++;
-                break;
+            case 'w': novo_x--; break; // Cima
+            case 's': novo_x++; break; // Baixo
+            case 'a': novo_y--; break; // Esquerda
+            case 'd': novo_y++; break; // Direita
+        }
+
+        // Verifica colisão com parede
+        if (mapas[fase_atual][novo_x][novo_y] != '#') {
+            pacman.posicao.x = novo_x;
+            pacman.posicao.y = novo_y;
+
+            // Verifica se Pac-Man comeu moeda
+            if (mapas[fase_atual][novo_x][novo_y] == '.') {
+                pontos += 10;
+                moedas_restantes--;
+                mapas[fase_atual][novo_x][novo_y] = ' ';
+            }
+
+            // Verifica se Pac-Man comeu fruta
+            if (mapas[fase_atual][novo_x][novo_y] == 'o') {
+                pontos += 50;
+                pacman.poderoso = 1;
+                frutas_tempo = 30; // Dura 30 ciclos
+                mapas[fase_atual][novo_x][novo_y] = ' ';
+            }
         }
     }
 }
-void mover_fantasmas(fantasmas *fantasmas, int num_fantasmas, int parede[10][35]) {
-    for (int i = 0; i < num_fantasmas; i++) {
-        int direcao = rand() % 4;
-        switch (direcao) {
-            case 0: // Cima
-                if (fantasmas[i].local.x > 0 && parede[fantasmas[i].local.x - 1][fantasmas[i].local.y] == 0)
-                    fantasmas[i].local.x--;
-                break;
-            case 1: // Esquerda
-                if (fantasmas[i].local.y > 0 && parede[fantasmas[i].local.x][fantasmas[i].local.y - 1] == 0)
-                    fantasmas[i].local.y--;
-                break;
-            case 2: // Baixo
-                if (fantasmas[i].local.x < 9 && parede[fantasmas[i].local.x + 1][fantasmas[i].local.y] == 0)
-                    fantasmas[i].local.x++;
-                break;
-            case 3: // Direita
-                if (fantasmas[i].local.y < 49 && parede[fantasmas[i].local.x][fantasmas[i].local.y + 1] == 0)
-                    fantasmas[i].local.y++;
-                break;
-        }
-    }
-}
-int main() {
-    int moedas_coletadas = 0, quant_moeda = 50;
-    int quant_frutas = 5, tempo_poder = 10;
-    int andar_cooldown = 500;
-    int score = 0;
-    int fase = 0;
-    int parede[10][35];
 
-    // Inicializa o mapa vazio
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 35; j++) {
-            parede[i][j] = 0;
-        }
-    }
-
-    // Criação das bordas
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 35; j++) {
-            parede[0][j] = 1;
-            parede[i][0] = 1;
-            parede[9][j] = 1;
-            parede[i][34] = 1;
-        }
-    }
-
-    fantasmas fantasma[4];
+void mover_fantasmas() {
     for (int i = 0; i < 4; i++) {
-        fantasma[i].local.x = rand() % 10;
-        fantasma[i].local.y = rand() % 50;
-        fantasma[i].vida = 1;
+        if (!fantasmas[i].vivo) continue;
+
+        int direcao = rand() % 4;
+        int novo_x = fantasmas[i].posicao.x;
+        int novo_y = fantasmas[i].posicao.y;
+
+        switch (direcao) {
+            case 0: novo_x--; break; // Cima
+            case 1: novo_x++; break; // Baixo
+            case 2: novo_y--; break; // Esquerda
+            case 3: novo_y++; break; // Direita
+        }
+
+        if (mapas[fase_atual][novo_x][novo_y] != '#' && (novo_x != pacman.posicao.x || novo_y != pacman.posicao.y)) {
+            fantasmas[i].posicao.x = novo_x;
+            fantasmas[i].posicao.y = novo_y;
+        }
     }
+}
 
-    frutas *fruta[quant_frutas];
-    moedas *moeda[quant_moeda];
-    pacman.local.x = 1;
-    pacman.local.y = 1;
+void verificar_colisoes() {
+    for (int i = 0; i < 4; i++) {
+        if (!fantasmas[i].vivo) continue;
 
+        if (fantasmas[i].posicao.x == pacman.posicao.x &&
+            fantasmas[i].posicao.y == pacman.posicao.y) {
+            if (pacman.poderoso) {
+                fantasmas[i].vivo = 0;
+                pontos += 200;
+            } else {
+                pacman.vidas--;
+                pacman.posicao.x = 1;
+                pacman.posicao.y = 1;
+
+                if (pacman.vidas <= 0) {
+                    printf("Game Over! Pontuação final: %d\n", pontos);
+                    exit(0);
+                }
+            }
+        }
+    }
+}
+
+void proxima_fase() {
+    fase_atual++;
+    if (fase_atual >= FASES) {
+        printf("Parabéns! Você completou o jogo com %d pontos!\n", pontos);
+        exit(0);
+    }
+    inicializar_jogo();
+}
+
+void atualizar_jogo() {
+    if (pacman.poderoso) {
+        frutas_tempo--;
+        if (frutas_tempo <= 0) {
+            pacman.poderoso = 0;
+        }
+    }
+    mover_pacman();
+    mover_fantasmas();
+    verificar_colisoes();
+
+    if (moedas_restantes == 0) {
+        proxima_fase();
+    }
+}
+
+int main() {
     srand(time(NULL));
+    inicializar_jogo();
 
-    // Inicializa moedas e frutas 
-    carregar_fase(fase, parede);
-    inicializar_moedas(moeda, quant_moeda, parede);
-    inicializar_frutas(fruta, quant_frutas, parede);
-   
-    // Loop principal do jogo
-    while (fase < 5) {
-        desenhar_mapa(parede, fantasma, moeda, fruta, quant_moeda, quant_frutas);
-        mover_pacman(parede); // Movimento do Pacman
-        mover_fantasmas(fantasma, 4, parede); // Movimento dos fantasmas
-
-        // Coleta moedas
-        for (int i = 0; i < quant_moeda; i++) {
-            if (moeda[i] && moeda[i]->local.x == pacman.local.x && moeda[i]->local.y == pacman.local.y) {
-                score += 10;
-                free(moeda[i]);
-                moeda[i] = NULL;
-                moedas_coletadas++;
-            }
-        }
-
-        // Coleta frutas
-        for (int i = 0; i < quant_frutas; i++) {
-            if (fruta[i] && fruta[i]->local.x == pacman.local.x && fruta[i]->local.y == pacman.local.y) {
-                score += 25;
-                free(fruta[i]);
-                fruta[i] = NULL;
-                pacman.poder = 1;
-                Sleep(tempo_poder * 1000);
-                pacman.poder = 0;
-            }
-        }
-
-        // Verifica progressão de fase
-        if (moedas_coletadas >= quant_moeda) {
-            printf("Você completou a fase %d!\n", fase + 1);
-            fase++;
-            moedas_coletadas = 0;
-            inicializar_moedas(moeda, quant_moeda, parede);
-            inicializar_frutas(fruta, quant_frutas, parede);
-        }
-
-        Sleep(200); // Tempo entre as atualizações (suave para o console)
+    while (1) {
+        desenhar_mapa();
+        atualizar_jogo();
+        Sleep(100); // Controle da velocidade
     }
-
-    printf("Parabéns! Você completou o jogo com %d pontos!\n", score);
     return 0;
 }
